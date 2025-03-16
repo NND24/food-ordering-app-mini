f8fb4d95d73380be351099ea74ec9622d7d3da3a
BuiAnh12 <buianh120403@gmail.com>
2025-03-16 19:25:44 +0700

    Patch: condition for fixing homepage

--- src/components/category/CategorySlider.jsx
+++ src/components/category/CategorySlider.jsx
@@ -8,53 +8,58 @@ import { useGetAllFoodTypesQuery } from "../../redux/features/foodType/foodTypeA
 
 const CategorySlider = () => {
   const { data: allFoodTypes, refetch: refetchAFoodTypes } = useGetAllFoodTypesQuery();
-
-  return (
-    <>
-      <div className='hidden sm:block'>
-        <Swiper
-          className='category-slider'
-          grabCursor={true}
-          breakpoints={{
-            320: {
-              slidesPerView: 3,
-              spaceBetween: 10,
-            },
-            640: {
-              slidesPerView: 6,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 6,
-              spaceBetween: 20,
-            },
-            1024: {
-              slidesPerView: 8,
-              spaceBetween: 25,
-            },
-            1280: {
-              slidesPerView: 10,
-              spaceBetween: 25,
-            },
-          }}
-        >
-          {allFoodTypes.map((type) => (
-            <SwiperSlide key={type._id}>
-              <CategoryItem type={type} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-
-      <div className='block sm:hidden'>
-        <div className='flex items-center gap-[15px] overflow-x-auto whitespace-nowrap'>
-          {allFoodTypes.map((type) => (
-            <CategoryItem key={type._id} type={type} />
-          ))}
+  if (allFoodTypes) {
+    return (
+      <>
+        <div className='hidden sm:block'>
+          <Swiper
+            className='category-slider'
+            grabCursor={true}
+            breakpoints={{
+              320: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+              },
+              640: {
+                slidesPerView: 6,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 6,
+                spaceBetween: 20,
+              },
+              1024: {
+                slidesPerView: 8,
+                spaceBetween: 25,
+              },
+              1280: {
+                slidesPerView: 10,
+                spaceBetween: 25,
+              },
+            }}
+          >
+            {allFoodTypes.map((type) => (
+              <SwiperSlide key={type._id}>
+                <CategoryItem type={type} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-      </div>
-    </>
-  );
+
+        <div className='block sm:hidden'>
+          <div className='flex items-center gap-[15px] overflow-x-auto whitespace-nowrap'>
+            {allFoodTypes.map((type) => (
+              <CategoryItem key={type._id} type={type} />
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+  else {
+    return (<></>)
+  }
+
 };
 
 export default CategorySlider;
